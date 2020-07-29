2af4d0d07ad448234fb3a3fdaa3f1b0a800135d9
unknown <sdgerbich@gmail.com>
2020-07-29 14:49:04 -0400

    day 2

--- techtalenttwitter/src/main/java/com/tts/techtalenttwitter/model/User.java
+++ techtalenttwitter/src/main/java/com/tts/techtalenttwitter/model/User.java
@@ -70,10 +70,11 @@ public class User {
     private Set<Role> roles;
 
     @ManyToMany(cascade = CascadeType.ALL)
-    @JoinTable(name = "user_follower", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "follower_id"))
+    @JoinTable(name = "user_follower", joinColumns = @JoinColumn(name = "user_id"), 
+    inverseJoinColumns = @JoinColumn(name = "follower_id"))
     private List<User> followers;
 
-    @ManyToMany(mappedBy = "followers")
+    @ManyToMany(mappedBy="followers")
     private List<User> following;
 
 }
